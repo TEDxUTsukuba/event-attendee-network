d9b9d2814fc480e0c30cda89586210c0c52bf634
makiart <haruki3968@gmail.com>
2024-05-04 12:08:48 +0900

    update: graphの見栄えを改良

--- src/app/visualizer/[eventId]/page.tsx
+++ src/app/visualizer/[eventId]/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { db } from "@/firebase/database";
-import { doc, collection, query, onSnapshot } from "firebase/firestore";
-import { useEffect, useState, useMemo } from "react";
+import { doc, collection, query, onSnapshot, addDoc } from "firebase/firestore";
+import { useEffect, useState, useMemo, useRef } from "react";
 import VisGraph, {
   GraphData,
   GraphEvents,
   Options,
+  Node,
+  Network,
 } from 'react-vis-graph-wrapper';
 
 
@@ -18,6 +21,16 @@ export default function VisualizeNetwork({
   const { eventId } = params;
   const [attendees, setAttendees] = useState([] as any[]);
   const [connections, setConnections] = useState([] as any[]);
+  const networkRef = useRef<Network | null>(null);
+
+  function getRandomColor() {
+    const letters = '89ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 8)];
+    }
+    return color;
+  }
 
   useEffect(() => {
     if (!eventId) return;
@@ -54,10 +67,14 @@ export default function VisualizeNetwork({
   }, [eventId]);
 
   const graphData = useMemo(() => {
-    const nodes = attendees.map((attendee) => ({
+    const nodes: Node[] = attendees.map((attendee) => ({
       id: attendee.id,
       label: attendee.name,
       // title: attendee.role,
+      // valueはconnectionの数によって変える
+      value: connections.filter((connection) => connection.parent_id === attendee.id).length,
+      color: getRandomColor(),
+
     }));
 
     const edges = connections.map((connection) => ({
@@ -71,35 +88,137 @@ export default function VisualizeNetwork({
     };
   }, [attendees, connections]);
 
-  const options = {
+  const options: Options = {
+    autoResize: true,
     layout: {
       hierarchical: false,
     },
     edges: {
-      color: '#000000',
+      color: {
+        color: '#000000',
+        highlight: '#fc5720',
+        opacity: 0.7,
+      },
+      smooth: {
+        enabled: true,
+        type: 'dynamic',
+        forceDirection: 'none',
+        roundness: 0.5,
+      },
+      arrows: {
+        to: {
+          enabled: true,
+          scaleFactor: 1.25,
+        },
+      },
+      width: 2.5,
+    },
+    nodes: {
+      shape: "dot",
+      scaling: {
+        label: {
+          min: 8,
+          max: 20,
+        },
+      },
+      font: {
+        background: 'rgba(255,255,255,0.8)',
+      },
+      color: {
+        border: '#000000',
+        background: '#666666',
+        highlight: {
+          border: '#fc5720',
+          background: '#ffcc00',
+        },
+      },
+      labelHighlightBold: true,
     },
     height: '100%',
     physics: {
       enabled: true,
       solver: 'forceAtlas2Based',
       forceAtlas2Based: {
-        gravitationalConstant: -26,
+        gravitationalConstant: -100,
         centralGravity: 0.005,
-        springLength: 230,
-        springConstant: 0.18,
-        avoidOverlap: 1.5,
+        springLength: 200,
+        springConstant: 0.5,
+        avoidOverlap: 3.5,
       },
-      maxVelocity: 146,
+      maxVelocity: 50,
       minVelocity: 0.75,
-      timestep: 0.5,
+      timestep: 0.1,
+      stabilization: {
+        enabled: true,
+        iterations: 100000,
+        updateInterval: 5000,
+        onlyDynamicEdges: true,
+        fit: true,
+      },
     },
     interaction: {
       zoomView: false,
     },
   };
 
+  const moveToOptions: any = {
+    animation: {
+      duration: 1000,
+      easingFunction: "easeInOutQuad",
+    }
+  };
+
+  const focusOptions: any = {
+    scale: 1.5,
+    animation: {
+      duration: 1000,
+      easingFunction: "easeInOutQuad",
+    }
+  };
+
+  const focusRandomNode = () => {
+    console.log('focusRandomNode')
+    console.log(attendees)
+    if (!networkRef.current) return;
+
+    const randomNode = attendees[Math.floor(Math.random() * attendees.length)];
+    if (!randomNode || !randomNode.id) return;
+
+    networkRef.current?.focus(randomNode.id, focusOptions);
+    networkRef.current?.selectNodes([randomNode.id]);
+  }
+
+  useEffect(() => {
+    const inter1 = setInterval(() => {
+      if (!networkRef.current) return;
+
+      if (Math.random() < 0.5) {
+        focusRandomNode();
+        console.log('focus');
+      } else {
+        networkRef.current?.unselectAll();
+        networkRef.current?.fit(moveToOptions);
+        networkRef.current?.startSimulation();
+        console.log('fit');
+      }
+    }, 5000);
+
+    // const inter2 = setInterval(() => {
+    //   if (!networkRef.current) return;
+    //   networkRef.current?.moveNode(attendees[Math.floor(Math.random() * attendees.length)].id, Math.random() * 100, Math.random() * 100);
+    // }, 500);
+
+    return () => {
+      clearInterval(inter1);
+      // clearInterval(inter2);
+    }
+
+  }, [attendees, connections]);
+
+
   return (
     <div>
+      <Button onClick={focusRandomNode} className="fixed left-3 top-3 z-50">Focus Random Node</Button>
       <h1>Event: {eventId}</h1>
       <h2>Attendees</h2>
       <ul>
@@ -119,7 +238,7 @@ export default function VisualizeNetwork({
       </ul>
       {graphData.nodes.length > 0 && (
         <div className="h-screen w-screen fixed left-0 top-0">
-          <VisGraph graph={graphData} options={options} style={{ height: '100%', width: '100%' }} />
+          <VisGraph ref={networkRef} graph={graphData} options={options} style={{ height: '100%', width: '100%' }} />
         </div>
       )}
     </div>
